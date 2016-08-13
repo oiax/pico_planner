4c740c63dabbba7992aaf28c8c6cdb38d386a24b
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-08-14 00:54:12 +0900

    Gem パッケージ annotate の導入

--- app/models/plan_item.rb
+++ app/models/plan_item.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: plan_items
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  points     :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class PlanItem < ApplicationRecord
 end
