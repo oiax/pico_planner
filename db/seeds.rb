5083e9ce6b5a1899137121e44324e4a9d1aee761
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-09-03 21:47:27 +0900

    PlanItem のシード(2)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
-item = PlanItems.new
+item = PlanItem.new
 item.name = '読書'
 item.description = '『走れメロス』を読む'
+item.save!
+
+item = PlanItem.new
+item.name = '買い物'
+item.description = '洗剤を買う'
 item.save!
 
 puts PlanItem.count
