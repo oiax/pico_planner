ec648dc062e1645ae79527a57141956d64783991
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-09-05 00:46:14 +0900

    PlanItem: Add starts_at and ends_at

--- db/migrate/20160813144526_create_plan_items.rb
+++ db/migrate/20160813144526_create_plan_items.rb
@@ -3,6 +3,8 @@ class CreatePlanItems < ActiveRecord::Migration[5.0]
     create_table :plan_items do |t|
       t.string :name, null: false
       t.text :description, null: false
+      t.datetime :starts_at, null: false
+      t.datetime :ends_at, null: false
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,24 @@
+time0 = Time.current.midnight
+
 item = PlanItem.new
 item.name = '読書'
 item.description = '『走れメロス』を読む'
+item.starts_at = time0.advance(days: 1, hours: 10)
+item.ends_at = time0.advance(days: 1, hours: 11)
 item.save!
 
 item = PlanItem.new
 item.name = '買い物'
 item.description = '洗剤を買う'
+item.starts_at = time0.advance(days: 1, hours: 16)
+item.ends_at = time0.advance(days: 1, hours: 16, minutes: 30)
 item.save!
 
 item = PlanItem.new
 item.name = '病院'
 item.description = '花粉症治療'
+item.starts_at = time0.advance(days: 2, hours: 13)
+item.ends_at = time0.advance(days: 2, hours: 14)
 item.save!
 
 puts PlanItem.count
