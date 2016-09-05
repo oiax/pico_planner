adc42a587bdcb4b4b3bef8db669ee7b290ef980e
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-09-06 01:17:10 +0900

    format_duration の改良 (2)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,22 @@
 module ApplicationHelper
   def format_duration(item)
-    s = item.starts_at.strftime('%Y-%m-%d %H:%M')
+    s = ''
+    if item.starts_at.year == Time.current.year
+      s << item.starts_at.strftime('%m-%d %H:%M')
+    else
+      s << item.starts_at.strftime('%Y-%m-%d %H:%M')
+    end
+
     s << ' &#x301c; '
-    s << item.ends_at.strftime('%Y-%m-%d %H:%M')
+
+    if item.ends_at.to_date == item.starts_at.to_date
+      s << item.ends_at.strftime('%H:%M')
+    elsif item.ends_at.year == item.starts_at.year
+      s << item.ends_at.strftime('%m-%d %H:%M')
+    else
+      s << item.ends_at.strftime('%Y-%m-%d %H:%M')
+    end
+
     s.html_safe
   end
 end
