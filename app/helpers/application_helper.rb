58d28f1c8a9d44af4267ca8d1c4ad3ffce85de79
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-09-10 00:37:38 +0900

    format_duration の改良 (4) 年月日

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,9 @@ module ApplicationHelper
 
     s = ''
     if item.starts_at.year == Time.current.year
-      s << item.starts_at.strftime("%m-%d (#{w1}) %H:%M")
+      s << item.starts_at.strftime("%-m月%-d日 (#{w1}) %H:%M")
     else
-      s << item.starts_at.strftime("%Y-%m-%d (#{w1}) %H:%M")
+      s << item.starts_at.strftime("%Y年%-m月%-d日 (#{w1}) %H:%M")
     end
 
     s << ' &#x301c; '
@@ -15,9 +15,9 @@ module ApplicationHelper
     if item.ends_at.to_date == item.starts_at.to_date
       s << item.ends_at.strftime('%H:%M')
     elsif item.ends_at.year == item.starts_at.year
-      s << item.ends_at.strftime("%m-%d (#{w2}) %H:%M")
+      s << item.ends_at.strftime("%-m月%-d日 (#{w2}) %H:%M")
     else
-      s << item.ends_at.strftime("%Y-%m-%d (#{w2}) %H:%M")
+      s << item.ends_at.strftime("%Y年%-m月%-d日 (#{w2}) %H:%M")
     end
 
     s.html_safe
