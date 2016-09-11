a621eea68cbbb18e091b25fdc56b97fbc5eddaa4
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-09-12 00:46:39 +0900

    plan_items#create 初期実装

--- app/controllers/plan_items_controller.rb
+++ app/controllers/plan_items_controller.rb
@@ -11,6 +11,13 @@ class PlanItemsController < ApplicationController
   end
 
   def create
+    @plan_item = PlanItem.new
+    @plan_item.name = params[:plan_item][:name]
+    @plan_item.description = params[:plan_item][:description]
+    @plan_item.starts_at = params[:plan_item][:starts_at]
+    @plan_item.ends_at = params[:plan_item][:ends_at]
+    @plan_item.save
+
     redirect_to :root
   end
 end
