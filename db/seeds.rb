9bfe3e148495cf0b51694480025d1b7c634f20f2
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-09-12 01:15:44 +0900

    db/seeds.rb の書き換え

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,24 @@
 time0 = Time.current.midnight
 
-item = PlanItem.new
-item.name = '読書'
-item.description = '『走れメロス』を読む'
-item.starts_at = time0.advance(days: 1, hours: 10)
-item.ends_at = time0.advance(days: 1, hours: 11)
-item.save!
+PlanItem.create(
+  name: '読書',
+  description: '『走れメロス』を読む',
+  starts_at: time0.advance(days: 1, hours: 10),
+  ends_at: time0.advance(days: 1, hours: 11)
+)
 
-item = PlanItem.new
-item.name = '買い物'
-item.description = '洗剤を買う'
-item.starts_at = time0.advance(days: 1, hours: 16)
-item.ends_at = time0.advance(days: 1, hours: 16, minutes: 30)
-item.save!
+PlanItem.create(
+  name: '買い物',
+  description: '洗剤を買う',
+  starts_at: time0.advance(days: 1, hours: 16),
+  ends_at: time0.advance(days: 1, hours: 16, minutes: 30)
+)
 
-item = PlanItem.new
-item.name = '帰省'
-item.description = "新幹線の指定席を取る。\nお土産を買う。"
-item.starts_at = time0.beginning_of_year.advance(years: 1, days: -2)
-item.ends_at = time0.beginning_of_year.advance(years: 1, days: 3)
-item.save!
+PlanItem.create(
+  name: '帰省',
+  description: "新幹線の指定席を取る。\nお土産を買う。",
+  starts_at: time0.beginning_of_year.advance(years: 1, days: -2),
+  ends_at: time0.beginning_of_year.advance(years: 1, days: 3)
+)
 
 puts PlanItem.count
