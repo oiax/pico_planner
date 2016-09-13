a93a2e04bb3b455e32bcdc2acea9855189a5cef1
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-09-13 22:07:38 +0900

    plan_items#edit, update, destroy への経路追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root 'top#index'
   get 'plan_items' => 'plan_items#index', as: :plan_items
   get 'plan_items/new' => 'plan_items#new', as: :new_plan_item
+  get 'plan_items/:id/edit' => 'plan_items#edit', as: :edit_plan_item
   post 'plan_items' => 'plan_items#create'
+  patch 'plan_items/:id' => 'plan_items#update', as: :plan_item
+  delete 'plan_items/:id' => 'plan_items#destroy'
 end
