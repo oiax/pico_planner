cfc6f739c6fa3425fcd77e9df54a94332824dd19
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-09-23 12:57:23 +0900

    config/routes.rb: resources :plan_items

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
   root 'top#index'
-  get 'plan_items' => 'plan_items#index', as: :plan_items
-  get 'plan_items/:id' => 'plan_items#show', as: :plan_item
-  get 'plan_items/new' => 'plan_items#new', as: :new_plan_item
-  get 'plan_items/:id/edit' => 'plan_items#edit', as: :edit_plan_item
-  post 'plan_items' => 'plan_items#create'
-  patch 'plan_items/:id' => 'plan_items#update'
-  delete 'plan_items/:id' => 'plan_items#destroy'
+  resources :plan_items
 end
