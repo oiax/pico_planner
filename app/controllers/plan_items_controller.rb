80b7218a75d89fae433f95517c0f1193b67f9ccb
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-10-12 11:16:22 +0900

    Chapter 12 (1)

--- app/controllers/plan_items_controller.rb
+++ app/controllers/plan_items_controller.rb
@@ -2,4 +2,8 @@ class PlanItemsController < ApplicationController
   def index
     @plan_items = PlanItem.order(:starts_at, :ends_at)
   end
+
+  def show
+    @plan_item = PlanItem.find(params[:id])
+  end
 end
