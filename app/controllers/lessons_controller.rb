0ee52e5069b0192832f47fffe57f21deeaad1040
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-11-17 23:55:31 +0900

    lessons#register アクション作成

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,8 @@
 class LessonsController < ApplicationController
   def form
   end
+
+  def register
+    @user_name = params[:user_name]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'top#index'
   get 'lessons/form' => 'lessons#form'
+  get 'lessons/register' => 'lessons#register'
   get 'plan_items' => 'plan_items#index', as: :plan_items
   get 'plan_items/:id' => 'plan_items#show', as: :plan_item
 end
