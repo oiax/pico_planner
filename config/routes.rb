b3a2bdb647409d532ea67d97e220317c9eb31a75
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-11-20 21:45:38 +0900

    plan_items#new へのリンク設置

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get 'lessons/register' => 'lessons#register'
   get 'plan_items' => 'plan_items#index', as: :plan_items
   get 'plan_items/:id' => 'plan_items#show', as: :plan_item
+  get 'plan_items/new' => 'plan_items#new', as: :new_plan_item
+  post 'plan_items' => 'plan_items#create'
 end
