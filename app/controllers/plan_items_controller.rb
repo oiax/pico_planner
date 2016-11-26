156401897c3b273838c6f5131706a884a83fab33
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-11-26 12:38:50 +0900

    plan_items#new に日時入力欄

--- app/controllers/plan_items_controller.rb
+++ app/controllers/plan_items_controller.rb
@@ -9,5 +9,8 @@ class PlanItemsController < ApplicationController
 
   def new
     @plan_item = PlanItem.new
+    time0 = Time.current.beginning_of_hour
+    @plan_item.starts_at = time0.advance(hours: 1)
+    @plan_item.ends_at = time0.advance(hours: 2)
   end
 end
