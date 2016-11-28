493852103304fc1853dc39cbd65bba848f0a0b43
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-11-28 11:34:19 +0900

    plan_items#create 最初の実装

--- app/controllers/plan_items_controller.rb
+++ app/controllers/plan_items_controller.rb
@@ -13,4 +13,15 @@ class PlanItemsController < ApplicationController
     @plan_item.starts_at = time0.advance(hours: 1)
     @plan_item.ends_at = time0.advance(hours: 2)
   end
+
+  def create
+    @plan_item = PlanItem.new
+    @plan_item.name = params[:plan_item][:name]
+    @plan_item.description = params[:plan_item][:description]
+    @plan_item.starts_at = params[:plan_item][:starts_at]
+    @plan_item.ends_at = params[:plan_item][:ends_at]
+    @plan_item.save!
+
+    redirect_to :plan_items
+  end
 end
