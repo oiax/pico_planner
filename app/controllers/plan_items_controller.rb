05880c4d3dcf8bb199801ff339eafe441ab8809d
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-12-01 04:06:24 +0900

    plan_items#edit, #update

--- app/controllers/plan_items_controller.rb
+++ app/controllers/plan_items_controller.rb
@@ -14,10 +14,22 @@ class PlanItemsController < ApplicationController
     @plan_item.ends_at = time0.advance(hours: 2)
   end
 
+  def edit
+    @plan_item = PlanItem.find(params[:id])
+  end
+
   def create
-    plan_item = PlanItem.create!(
-      params.require(:plan_item)
-        .permit(:name, :description, :starts_at, :ends_at)
+    PlanItem.create!(
+      params.permit(:name, :description, :starts_at, :ends_at)
+    )
+
+    redirect_to :plan_items
+  end
+
+  def update
+    plan_item = PlanItem.find(params[:id])
+    plan_item.update!(
+      params.permit(:name, :description, :starts_at, :ends_at)
     )
 
     redirect_to :plan_items
