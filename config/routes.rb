b270a06544e25f950b6a39b9a566a53bc79e8cb8
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-12-01 20:29:26 +0900

    Add lessons#hello action

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'top#index'
   get 'lessons/form' => 'lessons#form'
   get 'lessons/register' => 'lessons#register'
+  get 'lessons/hello' => 'lessons#hello'
   resources :plan_items
 end
