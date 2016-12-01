fb6465a75e3a82e498272ed30413539583c99677
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-12-01 23:57:04 +0900

    Bootstrap Modalによる削除確認

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require tether
+//= require bootstrap
 //= require moment
 //= require bootstrap-datetimepicker
 //= require moment/ja
