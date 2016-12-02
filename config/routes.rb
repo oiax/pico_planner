c986ad93cba84b97a1ddfc496aee9dfa2f91c7f0
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-12-02 12:21:31 +0900

    Add plan_items#destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   get 'lessons/hello' => 'lessons#hello'
   get 'plan_items' => 'plan_items#index', as: :plan_items
   get 'plan_items/new' => 'plan_items#new', as: :new_plan_item
-  get 'plan_items/:id/edit' => 'plan_items#edit', as: :edit_plan_item
   get 'plan_items/:id' => 'plan_items#show', as: :plan_item
+  get 'plan_items/:id/edit' => 'plan_items#edit', as: :edit_plan_item
   post 'plan_items' => 'plan_items#create'
   patch 'plan_items/:id' => 'plan_items#update'
+  delete 'plan_items/:id' => 'plan_items#destroy'
 end
