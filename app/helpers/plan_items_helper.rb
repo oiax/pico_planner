81b2da86f9738e554f11f9ab65147212b80a55e3
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-12-03 12:54:16 +0900

    Improve plan_items_nav_link helper

--- app/helpers/plan_items_helper.rb
+++ app/helpers/plan_items_helper.rb
@@ -7,7 +7,8 @@ module PlanItemsHelper
   def plan_items_nav_link(action)
     label = ACTION_LABEL_MAP[action]
     url = (action == :index ? :plan_items : [ action, :plan_items ])
-    klass = (action.to_s == params[:action] ? 'nav-link active' : 'nav-link')
-    link_to label, url, class: klass
+    css_classes = %w(nav-link)
+    css_classes << 'active' if action.to_s == params[:action]
+    link_to label, url, class: css_classes.join(' ')
   end
 end
