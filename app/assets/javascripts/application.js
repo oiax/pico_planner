0dfa538a9896733ef1dc0c2fe97fc237edca0f14
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-12-06 13:44:14 +0900

    Require tether and bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require tether
+//= require bootstrap-sprockets
 //= require moment
 //= require bootstrap-datetimepicker
 //= require moment/ja
