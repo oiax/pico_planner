09355d42897c95595d14d758bc5cf558f7b7cca2
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-12-11 11:14:46 +0900

    Modify datetime_picker.js

--- app/assets/javascripts/datetime_picker.js
+++ app/assets/javascripts/datetime_picker.js
@@ -1,15 +1,28 @@
 $(document).on('turbolinks:load', function() {
-  $('.datetime-picker').datetimepicker({
+  var icons = {
+    time: 'fa fa-clock-o',
+    date: 'fa fa-calendar',
+    up: 'fa fa-arrow-up',
+    down: 'fa fa-arrow-down',
+    previous: 'fa fa-chevron-left',
+    next: 'fa fa-chevron-right'
+  };
+  var keyBinds = {
+    up: null,
+    down: null,
+    left: null,
+    right: null
+  };
+  $('.date-picker').datetimepicker({
     locale: 'ja',
-    format: 'YYYY-MM-DD HH:mm',
+    format: 'YYYY-MM-DD',
     dayViewHeaderFormat: 'YYYY年 MMMM',
-    icons: {
-      time: 'fa fa-clock-o',
-      date: 'fa fa-calendar',
-      up: 'fa fa-arrow-up',
-      down: 'fa fa-arrow-down',
-      previous: 'fa fa-chevron-left',
-      next: 'fa fa-chevron-right'
-    }
+    icons: icons,
+    keyBinds: keyBinds
+  });
+  $('.time-picker').datetimepicker({
+    format: 'HH:mm',
+    icons: icons,
+    keyBinds: keyBinds
   });
 });
