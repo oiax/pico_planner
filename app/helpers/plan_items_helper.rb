d451d6f5792edfd3a718bac2314130665c577469
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-06-17 12:21:38 +0900

    [volume04] chapter 5

--- app/helpers/plan_items_helper.rb
+++ app/helpers/plan_items_helper.rb
@@ -11,4 +11,18 @@ module PlanItemsHelper
     css_classes << 'active' if action.to_s == params[:action]
     link_to label, url, class: css_classes.join(' ')
   end
+
+  def document_title
+    prefix =
+      case action_name
+      when 'index'; '予定表'
+      when 'of_today'; '今日の予定表'
+      when 'show'; '予定の詳細'
+      when 'new', 'create'; '予定の追加'
+      when 'edit', 'update'; '予定の変更'
+      else; raise
+      end
+
+    "#{prefix} | PicoPlanner"
+  end
 end
