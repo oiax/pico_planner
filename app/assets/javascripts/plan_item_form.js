41cefbb495ea3a2ef508d2b88b12fde630a0eb52
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-06-24 00:41:40 +0900

    [volume04] section 9.5

--- app/assets/javascripts/plan_item_form.js
+++ app/assets/javascripts/plan_item_form.js
@@ -2,6 +2,7 @@ $(document).on('turbolinks:load', function() {
   var form = $('form.new_plan_item, form.edit_plan_item');
 
   if (form.length > 0) {
-    console.log('Found!');
+    var allDay = $('#plan_item_all_day').prop('checked');
+    console.log(allDay);
   }
 });
