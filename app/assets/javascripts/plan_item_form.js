4f31ba8f4cb860ea5834a3f6bf80ab421847d5b5
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-06-24 13:19:16 +0900

    plan_item_form.js: function object

--- app/assets/javascripts/plan_item_form.js
+++ app/assets/javascripts/plan_item_form.js
@@ -2,7 +2,7 @@ $(document).on('turbolinks:load', function() {
   var form = $('form.new_plan_item, form.edit_plan_item');
 
   if (form.length > 0) {
-    function toggleInputFields() {
+    var toggleInputFields = function() {
       var allDay = $('#plan_item_all_day').prop('checked');
       form.find('.visible-if-all-day').toggle(allDay);
       form.find('.hidden-if-all-day').toggle(!allDay);
