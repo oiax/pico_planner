c7eda86e7969e8eb2c5069c043a1fa813c7376a5
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-06-24 13:17:41 +0900

    [volume04] section 9.9

--- app/assets/javascripts/plan_item_form.js
+++ app/assets/javascripts/plan_item_form.js
@@ -2,14 +2,14 @@ $(document).on('turbolinks:load', function() {
   var form = $('form.new_plan_item, form.edit_plan_item');
 
   if (form.length > 0) {
-    var allDay = $('#plan_item_all_day').prop('checked');
-    form.find('.visible-if-all-day').toggle(allDay);
-    form.find('.hidden-if-all-day').toggle(!allDay);
-
-    $('#plan_item_all_day').on('change', function() {
+    var toggleInputFields = function() {
       var allDay = $('#plan_item_all_day').prop('checked');
       form.find('.visible-if-all-day').toggle(allDay);
       form.find('.hidden-if-all-day').toggle(!allDay);
-    });
+    }
+
+    toggleInputFields();
+
+    $('#plan_item_all_day').on('change', toggleInputFields);
   }
 });
