27340536470b77cff024a1272b2574c460285862
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-07-10 19:37:27 +0900

    Refactor plan_item_form.js

--- app/assets/javascripts/plan_item_form.js
+++ app/assets/javascripts/plan_item_form.js
@@ -2,14 +2,16 @@ $(document).on('turbolinks:load', function() {
   var form = $('form.new_plan_item, form.edit_plan_item');
 
   if (form.length > 0) {
+    var checkBox = $('#plan_item_all_day')
+
     var toggleInputFields = function() {
-      var allDay = $('#plan_item_all_day').prop('checked');
+      var allDay = checkBox.prop('checked');
       form.find('.visible-if-all-day').toggle(allDay);
       form.find('.hidden-if-all-day').toggle(!allDay);
     }
 
     toggleInputFields();
 
-    $('#plan_item_all_day').on('change', toggleInputFields);
+    checkBox.on('change', toggleInputFields);
   }
 });
